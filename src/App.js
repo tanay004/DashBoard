94537dcc7bd2894c78f1e3fb7d91d777bf959367
Tanay <tanaykhandelwal007@gmail.com>
2023-07-30 21:50:24 +0530

    Pie and Bar chart

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
             <div className='flex relative dark:bg-main-dark-bg'>
                 <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
                     <TooltipComponent content="Settings" position='Top'>
-                        <button type="button" className=' text-3xl p-3 hover:drop-shadow-xl hover: bg-light-gray text-white' style={{backgroundColor:'blue', borderRadius: '50%'}}>
+                        <button type="button" className=' text-3xl p-3 hover:drop-shadow-xl hover: bg-light-gray text-white' style={{backgroundColor:'rgb(59,130,246)', borderRadius: '50%'}}>
                            <FiSettings/> 
                         </button>
                     </TooltipComponent>
@@ -30,13 +30,14 @@ const App = () => {
                         <Sidebar/> 
                     </div>
                 )}
-                <div className={`dark:bg-main-bg bg-main-bg min-h-screen
+                <div className={`dark:bg-main-bg bg-main-bg min-h-screen 
                     w-full ${activeMenu ? 'md:ml-72' : 'flex-2' } `}>
                     <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                         <Navbar/>
                     </div>
                 
                 <div>
+                    
                     <Routes>
                         {/*Dashboard*/}
                         <Route path="/" element={<Ecommerce/>}/>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
   const {activeMenu, setActiveMenu,screenSize} = useStateContext();
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white bg-blue-500 text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   const handleCloseSidebar = ()=>{
     if(activeMenu && screenSize<=900){
